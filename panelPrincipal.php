52f66fce9c55013ad2d00190bdadb13bee0cf278
BryanLR <108194629+bryanLRx@users.noreply.github.com>
2023-11-23 00:18:41 -0500

    feat: imprimir correctamente la lista

--- panelPrincipal.php
+++ panelPrincipal.php
@@ -4,65 +4,25 @@
     if(!isset($_SESSION["nombre"]) && !isset($_SESSION["clave"])){
         header("Location:index.php");
     }
-    
-    if(!isset($_COOKIE["idioma"])){
-        $file_es = "categorias_es.txt";
-        $fp_es = fopen($file_es, "r");
-        $contents_es = fread($fp_es, filesize($file_es));
-        
-        if($_GET["idioma"] == "es" && isset($_GET["idioma"])){
-            $file_es = "categorias_es.txt";
-            $fp_es = fopen($file_es, "r");
-            $contents_es = fread($fp_es, filesize($file_es));
-            header("panelPrincipal.php?idioma=es");
-        }
-    
-        if($_GET["idioma"] == "en" && isset($_GET["idioma"])){
-            $file_en = "categorias_en.txt";
-            $fp_en = fopen($file_en, "r");
-            $contents_en = fread($fp_en, filesize($file_en));
-            header("panelPrincipal.php?idioma=en");
-        }
-    } else {
+
+    $file_es = "categorias_es.txt";
+    $fp_es = fopen($file_es, "r");
+    $contents_es = fread($fp_es, filesize($file_es));
+
+    $file_en = "categorias_en.txt";
+    $fp_en = fopen($file_en, "r");
+    $contents_en = fread($fp_en, filesize($file_en));
+
+    if(isset($_COOKIE["idioma"])){
         if($_COOKIE["idioma"] == "es"){
-            $file_es = "categorias_es.txt";
-            $fp_es = fopen($file_es, "r");
-            $contents_es = fread($fp_es, filesize($file_es));
-        }
-    
-        if($_COOKIE["idioma"] == "en"){
-            $file_en = "categorias_en.txt";
-            $fp_en = fopen($file_en, "r");
-            $contents_en = fread($fp_en, filesize($file_en));
-        }
-    }
-/*
-    if(isset($_GET["idioma"])){
-        if($_GET["idioma"] == "es"){
-            $_COOKIE["idioma"] == "es";
             setcookie("idioma", "es");
             header("panelPrincipal.php?idioma=es");
-        } 
-
-        if($_GET["idioma"] == "en"){
-            $_COOKIE["idioma"] == "en";
+        }else if($_COOKIE["idioma"] == "en"){
             setcookie("idioma", "en");
             header("panelPrincipal.php?idioma=en");
-        } 
-    }
+        }
+    } 
 
-    if($_COOKIE["idioma"] == "es"){
-        $file_es = "categorias_es.txt";
-        $fp_es = fopen($file_es, "r");
-        $contents_es = fread($fp_es, filesize($file_es));
-    }
-
-    if($_COOKIE["idioma"] == "en"){
-        $file_en = "categorias_en.txt";
-        $fp_en = fopen($file_en, "r");
-        $contents_en = fread($fp_en, filesize($file_en));
-    }
-*/
     $nombre = $_SESSION['nombre'];
     $clave = $_SESSION['clave'];
 ?>
@@ -81,19 +41,26 @@
         <a href="panelPrincipal.php?idioma=es">ES (Español)</a>
         
         <a href="panelPrincipal.php?idioma=en">EN (Ingles)</a>
-
+        <br>
         <?php
-        echo $contents_es; 
-        if($_GET["idioma"] == "es" && isset($_GET["idioma"])) {
-            echo $contents_es; 
-        } 
-        if($_GET["idioma"] == "en" && isset($_GET["idioma"])) {
-            echo $contents_en;
+        if(!isset($_GET["idioma"])){
+            foreach(explode(",",$contents_es) as $name => $value){
+                echo $value."<br>"; 
+            }
+        }else{
+            if($_GET["idioma"]=="es"){
+                foreach(explode(",",$contents_es) as $name => $value){
+                    echo $value."<br>"; 
+                }
+            }else if($_GET["idioma"]=="en"){
+                foreach(explode(",",$contents_en) as $name => $value){
+                    echo $value."<br>"; 
+                }
+            }
         }
         ?>
 
         <br>
-        
         <a href="cerrarSesion.php?borrar=1">Cerrar Sesión</a>
     </body>
 </html>
